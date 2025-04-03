252ce580f7a161db61e3a02d1c3dc76ac3277e3a
Samuel-b10 <10samuelb2019@gmail.com>
2025-04-03 18:51:33 -0300

    Código da aula 03 - Contatos

--- Aula03/Celular/src/Main.java
+++ Aula03/Celular/src/Main.java
@@ -1,5 +1,51 @@
+import java.util.Scanner;
+import eletronico.Contato;
+
 public class Main {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Scanner scanner = new Scanner(System.in);
+        Contato[] contatos = new Contato[3];
+
+        // Criando três contatos
+        for (int i = 0; i < 3; i++) {
+
+            System.out.println("Digite o nome do contato " + (i + 1) + ":");
+            String nome = scanner.nextLine();
+
+            System.out.println("Digite o telefone do contato " + (i + 1) + ":");
+            String telefone = scanner.nextLine();
+
+            contatos[i] = new Contato(nome, telefone);
+        }
+
+        // Modificando os contatos
+        for (int i = 0; i < 3; i++) {
+
+            System.out.println("Deseja modificar o contato " + (i + 1) + "? (s/n)");
+            String resposta = scanner.nextLine();
+
+            if (resposta.equalsIgnoreCase("s")) {
+
+                System.out.println("Novo nome:");
+
+                contatos[i].setNome(scanner.nextLine());
+
+                System.out.println("Novo telefone:");
+
+                contatos[i].setTelefone(scanner.nextLine());
+            }
+        }
+
+        // Exibindo os contatos
+        System.out.println("\nLista de Contatos:");
+
+        for (Contato contato : contatos) {
+
+            System.out.println(contato);
+
+        }
+
     }
 }
